b2b04d0ab3d437c86f4431c3ae93fd035b1c7f9d
Artur Wiebe <artur@4wiebe.de>
2017-10-12 15:37:50 +0200

    updated licences on some af files

--- root/projectroot/usr/lib/gui/server.py
+++ root/projectroot/usr/lib/gui/server.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2016 Artur Wiebe <artur@4wiebe.de>
+# Copyright (c) 2017 Artur Wiebe <artur@4wiebe.de>
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
 # associated documentation files (the "Software"), to deal in the Software without restriction,
